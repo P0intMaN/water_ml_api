312f960e87a1feaa70cb3605d2d3a4e546aa84ba
CYBERDEVILZ <pranavcosmos4@gmail.com>
2022-05-16 10:35:23 +0530

    updated dataset

--- generate_dataset.py
+++ generate_dataset.py
@@ -12,13 +12,8 @@ leakage = ["no", "small", "big"]
 
 df = pd.DataFrame(columns=features)
 
-df['valve pressure'] = [ random.randint(0,400) for x in range(0, 100_000)]
-
-df['leakage'] = random.choices(
-    leakage,
-    weights=(67,20,12),
-    k=total_size
-)
+df['valve pressure'] = [ random.randint(0,100) for x in range(0, 33_000)]+[ random.randint(100, 250) for x in range(0, 33_000)]+[ random.randint(250, 1000) for x in range(0, 34_000)]
+df["leakage"] = ["no" for i in range(0, 33_000)] + ["small" for i in range(0, 33_000)] + ["big" for i in range(0, 34_000)]
 
 df.to_csv('leakage_dataset.csv', index=False)
 # print(df.head())
