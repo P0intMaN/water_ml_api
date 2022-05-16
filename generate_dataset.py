04e7a00d44847db78fc1ece5ee29eb244279fbf8
Pratheek U <pratheekastro3@gmail.com>
2022-05-16 10:44:29 +0530

    add leakage_dataset

--- generate_dataset.py
+++ generate_dataset.py
@@ -12,13 +12,19 @@ leakage = ["no", "small", "big"]
 
 df = pd.DataFrame(columns=features)
 
-df['valve pressure'] = [ random.randint(0,400) for x in range(0, 100_000)]
+no_valve_pressure = [ random.randint(0,100) for x in range(0, 40_000)]
+small_valve_pressure = [ random.randint(100,200) for x in range(0, 30_000)]
+big_valve_pressure = [ random.randint(200,300) for x in range(0, 30_000)]
 
-df['leakage'] = random.choices(
-    leakage,
-    weights=(67,20,12),
-    k=total_size
-)
+leakage_values = no_valve_pressure + small_valve_pressure + big_valve_pressure
+
+leakage_types = ['no' for x in range(0, 40_000)] + ['small' for x in range(0, 30_000)] + ['big' for x in range(0, 30_000)]
+
+df['valve pressure'] = leakage_values
+df['leakage'] = leakage_types
+
 
 df.to_csv('leakage_dataset.csv', index=False)
-# print(df.head())
+
+
+
